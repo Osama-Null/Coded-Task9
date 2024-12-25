3a5d96a8165c722f66ffbaf7e32ce02691feb58d
Osama <osamaashkanani@gmail.com>
2024-12-25 12:03:51 +0300

    Add project files.

---
+++ Models/Accounts.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleT9.Models
+{
+    internal class Account
+    {
+        private string userName;
+        public string UserName
+        {
+            get
+            {
+                return userName;
+            }
+            set
+            {
+                if (value != null)
+                {
+                    userName = value;
+                }
+                else Console.WriteLine("Please Enter User Name: ");
+            }
+        }
+        private float salary;
+        public float Salary 
+        {
+            get 
+            { 
+                return salary;
+            }
+            set 
+            {
+                if(value >=  350 && value <= 900)
+                {
+                    salary = value;
+                }
+                else Console.WriteLine("Out of range!");
+            } 
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"\n\nAll valid inputs!\n\nYour username: {userName}\nYour salary: {salary}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using ConsoleT9.Models;
+
+Account account = new Account();
+
+string usrName;
+float usrSalary;
+
+Console.Write("Enter username: ");
+account.UserName = Console.ReadLine();
+
+Console.Write("Enter salary: ");
+account.Salary = Convert.ToInt32(Console.ReadLine());
+
+account.GetInfo();
+
